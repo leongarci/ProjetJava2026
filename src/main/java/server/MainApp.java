5dbcb57fedd088591362d57b52777167c901487c
leongarci <leongarcia1669@gmail.com>
2026-05-13 14:31:20 +0200

    modif gitignore et mainapp

--- src/main/java/server/MainApp.java
+++ src/main/java/server/MainApp.java
@@ -1,5 +1,8 @@
-import server.MosquittoApp;
+package server;
 
-public static void main(String[] args) throws Exception {
-    new MosquittoApp("app-client");
+public class MainApp {
+
+    public static void main(String[] args) throws Exception {
+        new MosquittoApp("app-client");
+    }
 }
